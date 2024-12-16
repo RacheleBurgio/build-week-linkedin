6193b619844f93092b3d23f69b51022e949723ba
Sara Severgnini <sara.severgnini.05@gmail.com>
2024-12-16 14:58:07 +0100

    new

--- src/components/ColonnaCentraleAlto.jsx
+++ src/components/ColonnaCentraleAlto.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/custom-bootstrap.css'
 
-const ColonnaCentraleAlto = () => {
+const ColonnaCentraleAlto = ({profile}) => {
   return (
     <div className="container mt-4">
     <div className="card mb-4">
@@ -26,8 +26,68 @@ const ColonnaCentraleAlto = () => {
         </div>
       </div>
     </div>
+
+    {/* Sezione Consigliati per Te */}
+    <RecommendedForYou />
+
+    {/* Sezione Analisi */}
+    <Analytics />
+
+    {/* Sezione Attività */}
+    <Activity />
   </div>
-  )
+);
+};
+
+// Componente Consigliati per Te
+const RecommendedForYou = () => {
+return (
+  <div className="card mb-4">
+    <div className="card-header">
+      <h5>Consigliati per te</h5>
+    </div>
+    <div className="card-body">
+      <ul className="list-group">
+        <li className="list-group-item">Contatto 1</li>
+        <li className="list-group-item">Contatto 2</li>
+        <li className="list-group-item">Contatto 3</li>
+      </ul>
+    </div>
+  </div>
+);
+};
+
+// Componente Analisi
+const Analytics = () => {
+return (
+  <div className="card mb-4">
+    <div className="card-header">
+      <h5>Analisi</h5>
+    </div>
+    <div className="card-body">
+      <p>Visualizzazioni del profilo: 123</p>
+      <p>Visualizzazioni dei post: 456</p>
+    </div>
+  </div>
+);
+};
+
+// Componente Attività
+const Activity = () => {
+return (
+  <div className="card mb-4">
+    <div className="card-header">
+      <h5>Attività</h5>
+    </div>
+    <div className="card-body">
+      <ul className="list-group">
+        <li className="list-group-item">Hai commentato un post</li>
+        <li className="list-group-item">Hai condiviso un articolo</li>
+        <li className="list-group-item">Hai aggiunto una nuova competenza</li>
+      </ul>
+    </div>
+  </div>
+);
 }
 
 export default ColonnaCentraleAlto
